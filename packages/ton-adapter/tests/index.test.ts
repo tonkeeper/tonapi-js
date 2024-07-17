d0f131e61153e33659f5392ef48eb3748c4de109
Moiseev Ilya <ilya@mois.pro>
2024-07-17 17:53:24 +0200

    refactor: eslint in ton-adapter tests

--- packages/ton-adapter/tests/index.test.ts
+++ packages/ton-adapter/tests/index.test.ts
@@ -1,13 +1,7 @@
-import {
-    Address,
-    WalletContractV4
-} from '@ton/ton';
+import { Address, WalletContractV4 } from '@ton/ton';
 import { mnemonicNew, mnemonicToPrivateKey } from '@ton/crypto';
 import { WalletItem } from './utils/contract';
-import {
-    clientTonApi,
-    getTonCenterClient
-} from './utils/clients';
+import { clientTonApi, getTonCenterClient } from './utils/clients';
 
 const client = process.env.CLIENT === 'toncenter' ? getTonCenterClient() : clientTonApi;
 
@@ -45,10 +39,14 @@ test('Missing wallet contract', async () => {
 test('Uninit address with balance', async () => {
     // const address = Address.parse('EQBeNSukqcF7a27a-kq3R7xFjEa9w1vd2HpQ0NowlnHq6UqC');
     // const rawAddress = '0:5e352ba4a9c17b6b6edafa4ab747bc458c46bdc35bddd87a50d0da309671eae9';
-    const publicKey = 57787885441719996105546335440755198603738413181862234174694273066261933925918n;
-    
+    const publicKey =
+        57787885441719996105546335440755198603738413181862234174694273066261933925918n;
+
     // create wallet contract
-    const wallet = WalletContractV4.create({ workchain: 0, publicKey: Buffer.from(publicKey.toString(16), 'hex')});
+    const wallet = WalletContractV4.create({
+        workchain: 0,
+        publicKey: Buffer.from(publicKey.toString(16), 'hex')
+    });
     const contract = client.open(wallet);
 
     // Get balance
@@ -61,10 +59,14 @@ test('Uninit address with balance', async () => {
 test('Uninit address without balance', async () => {
     // const address = Address.parse('EQAta6RYppvVkEavFszcZKFx9q1yobABP3vY5RE36DQxv6eO');
     // const rawAddress = '0:2d6ba458a69bd59046af16ccdc64a171f6ad72a1b0013f7bd8e51137e83431bf';
-    const publicKey = 103331518834641293154200435092860708617866223941720484731223285872059976834397n;
-    
+    const publicKey =
+        103331518834641293154200435092860708617866223941720484731223285872059976834397n;
+
     // create wallet contract
-    const wallet = WalletContractV4.create({ workchain: 0, publicKey: Buffer.from(publicKey.toString(16), 'hex')});
+    const wallet = WalletContractV4.create({
+        workchain: 0,
+        publicKey: Buffer.from(publicKey.toString(16), 'hex')
+    });
     const contract = client.open(wallet);
 
     // Get balance
