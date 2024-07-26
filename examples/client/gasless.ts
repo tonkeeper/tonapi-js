3268d995d3581c450302904ec3968d4036e4d319
Max Voloshinskii <me@voloshinskii.ru>
2024-07-26 19:03:18 +0300

    fixes

--- examples/client/gasless.ts
+++ examples/client/gasless.ts
@@ -1,7 +1,7 @@
 import { Api, TonApiClient } from '@ton-api/client';
-import { WalletContractV5R1 } from '@ton/ton';
-import { Address, SendMode, beginCell, internal, Cell, comment, toNano } from '@ton/core';
-import { mnemonicToPrivateKey } from '@ton/crypto';
+import { WalletContractV5R1 } from "@ton/ton";
+import { Address, SendMode, beginCell, internal, Cell, toNano, storeMessage, external } from '@ton/core';
+import {mnemonicToPrivateKey, sign} from '@ton/crypto';
 import { ContractAdapter } from '@ton-api/ton-adapter';
 
 const http = new TonApiClient({
@@ -28,44 +28,34 @@ export interface CreateJettonTransferPayloadParams {
     /* Receiver's address */
     receiverAddress: Address;
     jettonAmount: number | bigint;
-    forwardBody?: Cell | string;
+    forwardBody?: Cell;
     /* Query id */
     queryId?: number | bigint;
-}
-
-function getWalletQueryId() {
-    const tonkeeperSignature = (0x546de4ef).toString(16);
-    const value = Buffer.concat([
-        Buffer.from(tonkeeperSignature, 'hex'),
-        Buffer.from(tonkeeperSignature, 'hex'),
-    ]);
-    return BigInt('0x' + value.toString('hex'));
 }
 
 function makeJettonTransferPayload(createJettonTransferPayloadParams: CreateJettonTransferPayloadParams) {
     return beginCell()
       .storeUint(OP_CODES.JETTON_TRANSFER, 32)
       .storeUint(
-        createJettonTransferPayloadParams.queryId,
+        createJettonTransferPayloadParams.queryId ?? 0,
         64,
       )
       .storeCoins(createJettonTransferPayloadParams.jettonAmount)
       .storeAddress(createJettonTransferPayloadParams.receiverAddress)
       .storeAddress(createJettonTransferPayloadParams.excessesAddress)
       .storeBit(false) // null custom_payload
-      .storeCoins(createJettonTransferPayloadParams.forwardAmount ?? 1n)
-      .storeMaybeRef(typeof createJettonTransferPayloadParams.forwardBody === 'string' ? comment(createJettonTransferPayloadParams.forwardBody) : createJettonTransferPayloadParams.forwardBody)
+      .storeCoins(createJettonTransferPayloadParams.forwardAmount ?? BigInt(1))
+      .storeMaybeRef(createJettonTransferPayloadParams.forwardBody)
       .endCell();
 }
 
-// Amount for jetton transfer. Usually 0.05 TON is enough for most jetton transfers without forwardBody
-const BASE_JETTON_SEND_AMOUNT = toNano(0.05);
+// Amount for jetton transfer. Usually 0.1 TON is enough for most jetton transfers without forwardBody
+const BASE_JETTON_SEND_AMOUNT = toNano(0.1);
 
 const main = async () => {
     const mnemonic = 'around front fatigue cabin december maximum coconut music pride animal series course comic adjust inject swift high wage maid myself grass act bicycle credit'; // replace with a correct your mnemonic phrase
     const destination = Address.parse('EQAta6RYppvVkEavFszcZKFx9q1yobABP3vY5RE36DQxv6eO'); // replace with a correct recipient address
     const usdtMaster = Address.parse('EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs'); // USDt jetton master.
-    const usdtTransferAmount = 1000000n; // amount in nanocoins. 1 USDt.
 
     const keyPair = await mnemonicToPrivateKey(mnemonic.split(' '));
     const workChain = 0;
@@ -85,16 +75,15 @@ const main = async () => {
     const relayerAddress = await printConfigAndReturnRelayAddress();
 
     const tetherTransferPayload = makeJettonTransferPayload({
-        queryId: getWalletQueryId(),
-        jettonAmount: usdtTransferAmount,
+        queryId: 0,
+        jettonAmount: BigInt(1),
         excessesAddress: relayerAddress,
         receiverAddress: destination,
-        forwardBody: 'It\'s my gasless USDT transfer!'
     });
 
     const relayerTransferMock = makeJettonTransferPayload({
-        queryId: getWalletQueryId(),
-        jettonAmount: 1n,
+        queryId: 0,
+        jettonAmount: BigInt(1),
         forwardAmount: 0,
         excessesAddress: relayerAddress,
         receiverAddress: relayerAddress,
@@ -105,6 +94,7 @@ const main = async () => {
     const transfer = wallet.createTransfer({
         // For gasless we should build and sign ext in transfer
         authType: 'internal',
+        timeout: Math.floor(Date.now() / 1e3) + 300,
         // A valid secret key is not necessary for emulation. You can just pass a random buffer
         secretKey: keyPair.secretKey,
         sendMode: SendMode.PAY_GAS_SEPARATELY + SendMode.IGNORE_ERRORS,
@@ -124,13 +114,23 @@ const main = async () => {
           })],
     });
 
-    console.log(transfer.toBoc().toString('base64'));
-
+    const ext =  beginCell()
+      .storeWritable(
+        storeMessage(
+          external({
+            to: contract.address,
+            init: seqno === 0 ? contract.init : undefined,
+            body: transfer,
+          }),
+        ),
+      )
+      .endCell();
+    
     const params = await client.gasless.gaslessEstimate(usdtMaster, {
         walletAddress: address,
         walletPublicKey: keyPair.publicKey.toString('hex'),
         messages: [{
-            boc: transfer,
+            boc: ext,
         }]
     }).catch(async (res) => console.log(await res.json()));
 };
